491da7610133ade26521e39e00b2658f19286a99
christophesauvage62@gmail.com <christophesauvage62@gmail.com@c689e736-add6-09a2-d08d-884fce03481e>
2013-11-26 17:50:21 +0000

    Création des répertoires + grapheInverse() + poidsArete() + TypGraphePERT

---
+++ SRC/pert.c
@@ -0,0 +1,22 @@
+#include "libgraphe.h"
+#include "pert.h"
+
+
+TypGraphe* listeSucc(TypGraphe* graphe, int sommet){}
+
+int dateAuplusTot(TypGraphePERT* graphe, int sommet){
+	/*int tempsSommet= 0;
+	if(listeSucc(graphe,sommet == 0)){
+		tempsSommet= 0;
+	}else if()*/
+
+
+}
+
+int dateAuplusTart(TypGraphePERT* graphe, int sommet){}
+
+int plusCourtChemin(TypGraphePERT* graphe, int depart, int arrivee){}
+
+int* algoBellmanFord(TypGraphePERT* graphe, int depart){}
+
+TypVoisins* cheminCritique(TypGraphePERT* graphe){}
