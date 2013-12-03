9ca3f10addfe8376f0d8f71568452aa3c009e040
christophesauvage62@gmail.com <christophesauvage62@gmail.com@c689e736-add6-09a2-d08d-884fce03481e>
2013-12-03 13:15:10 +0000

    Ajout afficherCheminCritique() + lireGraphePERT()

--- SRC/pert.c
+++ SRC/pert.c
@@ -1,6 +1,7 @@
-#include <stdlib.h>
+ï»¿#include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #include "libgraphe.h"
 #include "pert.h"
 
@@ -13,18 +14,18 @@ static char sommetEnTache(int);
 	/*
 	* Fonction : creerTache
 	*
-	* Paramètres : char nom, le nom de la tâche
-	*              char *intitule, l'intitulé de la tâche
-	*              char *dependances, les prédecesseurs de la tâche
-	*                  (ou chaîne vide s'il n'y en a pas)
+	* ParamÃ¨tres : char nom, le nom de la tÃ¢che
+	*              char *intitule, l'intitulÃ© de la tÃ¢che
+	*              char *dependances, les prÃ©decesseurs de la tÃ¢che
+	*                  (ou chaÃ®ne vide s'il n'y en a pas)
 	*
-	* Retour : TypTache*, pointeur sur la tâche créée
+	* Retour : TypTache*, pointeur sur la tÃ¢che crÃ©Ã©e
 	*
-	* Description : Crée une tâche dont les informations sont passées
-	*               en paramètre. La mémoire est allouée dans la fonction.
+	* Description : CrÃ©e une tÃ¢che dont les informations sont passÃ©es
+	*               en paramÃ¨tre. La mÃ©moire est allouÃ©e dans la fonction.
 	*/
 TypTache* creerTache(char nom, char *intitule, int duree, char *dependances) {
-	TypTache *tache;  /* La nouvelle tâche créée */
+	TypTache *tache;  /* La nouvelle tÃ¢che crÃ©Ã©e */
 	
 	tache = malloc(sizeof(TypTache));
 	tache->nom = nom;
@@ -39,23 +40,23 @@ TypTache* creerTache(char nom, char *intitule, int duree, char *dependances) {
 	/*
 	* Fonction : creerGraphePERT
 	*
-	* Paramètres : TypTache **taches, un tableau de tâches
+	* ParamÃ¨tres : TypTache **taches, un tableau de tÃ¢ches
 	*              int nbTaches, la taille du tableau taches
 	*
-	* Retour : TypGraphePERT*, pointeur sur le graphe PERT créé
+	* Retour : TypGraphePERT*, pointeur sur le graphe PERT crÃ©Ã©
 	*
-	* Description : Crée un graphe PERT à partir d'un tableau de tâches.
-	*               Les tâches alpha et oméga sont créées dans la fonction.
+	* Description : CrÃ©e un graphe PERT Ã  partir d'un tableau de tÃ¢ches.
+	*               Les tÃ¢ches alpha et omÃ©ga sont crÃ©Ã©es dans la fonction.
 	*/
 TypGraphePERT* creerGraphePERT(TypTache **taches, int nbTaches) {
-	TypGraphePERT *graphePERT;               /* Le graphe PERT créé */
-	TypGraphe     *graphe;                   /* Le graphe associé */
-	TypTache      *tacheDepart;              /* La tâche alpha de départ */
-	TypTache      *tacheArrivee;             /* La tâche oméga d'arrivée */
-	char          *dependances;               /* Les dépendances d'une tâche */
-	int           sommet;                    /* Le numéro d'un sommet */
-	bool          sansSuccesseur[nbTaches];  /* si [i] = true, alors la tâche i+1 
-												est à relier au sommet oméga */
+	TypGraphePERT *graphePERT;               /* Le graphe PERT crÃ©Ã© */
+	TypGraphe     *graphe;                   /* Le graphe associÃ© */
+	TypTache      *tacheDepart;              /* La tÃ¢che alpha de dÃ©part */
+	TypTache      *tacheArrivee;             /* La tÃ¢che omÃ©ga d'arrivÃ©e */
+	char          *dependances;               /* Les dÃ©pendances d'une tÃ¢che */
+	int           sommet;                    /* Le numÃ©ro d'un sommet */
+	bool          sansSuccesseur[nbTaches];  /* si [i] = true, alors la tÃ¢che i+1 
+												est Ã  relier au sommet omÃ©ga */
 	int           i;                         /* Permet le parcours des boucles */
 	int           j;                         /* Permet le parcours des boucles */
 	
@@ -64,19 +65,19 @@ TypGraphePERT* creerGraphePERT(TypTache **taches, int nbTaches) {
 		sansSuccesseur[i] = true;
 	}
 	
-	/* Création du graphe associé au graphe PERT */
+	/* CrÃ©ation du graphe associÃ© au graphe PERT */
 	graphe = creerGraphe(nbTaches + 2);
 	
-	/* Création des sommets */
+	/* CrÃ©ation des sommets */
 	for (i = 1; i <= nbTaches + 2; i++) {
 		insertionSommet(graphe,i);
 	}
 	
-	/* Création des tâches alpha et oméga */
+	/* CrÃ©ation des tÃ¢ches alpha et omÃ©ga */
 	tacheDepart = creerTache(sommetEnTache(nbTaches+1),'\0',0,'\0');
 	tacheArrivee = creerTache(sommetEnTache(nbTaches+2),'\0',0,'\0');
 	
-	/* Création des arêtes */
+	/* CrÃ©ation des arÃªtes */
 	for (i = 1; i <= nbTaches; i++) {
 		j = 0;
 		dependances = taches[i-1]->dependances;
@@ -88,25 +89,25 @@ TypGraphePERT* creerGraphePERT(TypTache **taches, int nbTaches) {
 			j++;
 		}
 		
-		/* Si la tâche n'a pas de prédecesseur, on la relie à alpha */
+		/* Si la tÃ¢che n'a pas de prÃ©decesseur, on la relie Ã  alpha */
 		if (j == 0) {
 			insertionAreteOriente(graphe,nbTaches+1,i,0);
 		}
 	}
 	
-	/* On relie les tâches sans successeurs au sommet oméga */
+	/* On relie les tÃ¢ches sans successeurs au sommet omÃ©ga */
 	for (i = 0; i <= nbTaches - 1; i++) {
 		if (sansSuccesseur[i] == true) {
 			insertionAreteOriente(graphe,i+1,nbTaches+2,taches[i]->duree);
 		}
 	}
 	
-	/* On ajoute les tâches alpha et oméga au tableau des tâches */
+	/* On ajoute les tÃ¢ches alpha et omÃ©ga au tableau des tÃ¢ches */
 	taches = realloc(taches,(nbTaches+2) * sizeof(TypTache));
 	taches[nbTaches] = tacheDepart;
 	taches[nbTaches+1] = tacheArrivee;
 	
-	/* Création du graphe PERT */
+	/* CrÃ©ation du graphe PERT */
 	graphePERT = malloc(sizeof(TypGraphePERT));
 	graphePERT->graphe = graphe;
 	graphePERT->taches = taches;
@@ -118,19 +119,19 @@ TypGraphePERT* creerGraphePERT(TypTache **taches, int nbTaches) {
 	/*
 	* Fonction : deleteGraphePERT
 	*
-	* Paramètres : TypGraphePERT *graphePERT, pointeur sur un graphe PERT
+	* ParamÃ¨tres : TypGraphePERT *graphePERT, pointeur sur un graphe PERT
 	*
-	* Description : Supprime le graphe PERT passé en paramètre. Toute la
-	*				mémoire qui lui avait été allouée est libérée.
+	* Description : Supprime le graphe PERT passÃ© en paramÃ¨tre. Toute la
+	*				mÃ©moire qui lui avait Ã©tÃ© allouÃ©e est libÃ©rÃ©e.
 	*/
 void deleteGraphePERT(TypGraphePERT *graphePERT) {
-	/* Suppression du graphe associé */
+	/* Suppression du graphe associÃ© */
 	deleteGraphe(graphePERT->graphe);
 	
-	/* Libération de la mémoire occupée par les tâches */
+	/* LibÃ©ration de la mÃ©moire occupÃ©e par les tÃ¢ches */
 	free(graphePERT->taches);
 	
-	/* Libération de la mémoire occupée par le graphe PERT */
+	/* LibÃ©ration de la mÃ©moire occupÃ©e par le graphe PERT */
 	free(graphePERT);
 }
 
@@ -138,10 +139,10 @@ void deleteGraphePERT(TypGraphePERT *graphePERT) {
 	/*
 	* Fonction : calculDates
 	*
-	* Paramètres : TypGraphePERT *graphePERT, pointeur sur un graphe PERT
+	* ParamÃ¨tres : TypGraphePERT *graphePERT, pointeur sur un graphe PERT
 	*
-	* Description : Calcule les dates au plus tôt et au plus tard 
-	*				de toutes les tâches du graphe PERT passé en paramètre
+	* Description : Calcule les dates au plus tÃ´t et au plus tard 
+	*				de toutes les tÃ¢ches du graphe PERT passÃ© en paramÃ¨tre
 	*/
 void calculDates(TypGraphePERT *graphePERT) {
 	calculDatesAuPlusTot(graphePERT,graphePERT->graphe->nbrMaxSommets-1);
@@ -152,22 +153,22 @@ void calculDates(TypGraphePERT *graphePERT) {
 	/*
 	* Fonction : calculDatesAuPlusTot
 	*
-	* Paramètres : TypGraphePERT *graphePERT, pointeur sur un graphe PERT
-	*              int sommetDepart, le numéro du sommet de départ
+	* ParamÃ¨tres : TypGraphePERT *graphePERT, pointeur sur un graphe PERT
+	*              int sommetDepart, le numÃ©ro du sommet de dÃ©part
 	*
-	* Description : Calcule les dates au plus tôt d'un graphe de toutes les
-	*				tâches du graphe PERT passé en paramètre.
-	*				Pour faire cela, on implémente l'algo de Bellman-Ford dans
-	*				sa version de calcul de plus long chemin à origine unique.
+	* Description : Calcule les dates au plus tÃ´t d'un graphe de toutes les
+	*				tÃ¢ches du graphe PERT passÃ© en paramÃ¨tre.
+	*				Pour faire cela, on implÃ©mente l'algo de Bellman-Ford dans
+	*				sa version de calcul de plus long chemin Ã  origine unique.
 	*/
 static void calculDatesAuPlusTot(TypGraphePERT *graphePERT, int sommetDepart) {
-	TypGraphe *graphe;   /* Le graphe associé au graphe PERT */
-	TypTache  **taches;  /* Les tâches associées au graphe PERT */
+	TypGraphe *graphe;   /* Le graphe associÃ© au graphe PERT */
+	TypTache  **taches;  /* Les tÃ¢ches associÃ©es au graphe PERT */
 	int       nbSommets; /* Le nombre de sommets du graphe */
 	int       i;         /* Permet le parcours des sommets du graphe */
 	int       j;         /* Permet le parcours des sommets du graphe */
-	int       nbIter;    /* Le nombre d'itérations de la boucle effectué */
-	bool      modif;     /* false si aucune nouvelle date n'a été calculée */
+	int       nbIter;    /* Le nombre d'itÃ©rations de la boucle effectuÃ© */
+	bool      modif;     /* false si aucune nouvelle date n'a Ã©tÃ© calculÃ©e */
 	
 	graphe = graphePERT->graphe;
 	taches = graphePERT->taches;
@@ -175,7 +176,7 @@ static void calculDatesAuPlusTot(TypGraphePERT *graphePERT, int sommetDepart) {
 	
 	/* Initialisation */
 	for (i = 1; i <= nbSommets; i++) {
-		/* Si ce n'est pas la tâche de départ (alpha) */
+		/* Si ce n'est pas la tÃ¢che de dÃ©part (alpha) */
 		if (i != sommetDepart) {
 			taches[i-1]->dateTot = -1;
 		}
@@ -187,17 +188,17 @@ static void calculDatesAuPlusTot(TypGraphePERT *graphePERT, int sommetDepart) {
 	nbIter = 1;
 	modif = true;
 	
-	/* Calcul des dates au plus tôt de chaque tâche */
+	/* Calcul des dates au plus tÃ´t de chaque tÃ¢che */
 	while ( (nbIter < nbSommets + 1) && (modif == true) ) {
 		modif = false;
 		
-		/* Pour chaque arête du graphe... */
+		/* Pour chaque arÃªte du graphe... */
 		for (i = 1; i <= nbSommets; i++) {
 			for (j = 1; j <= nbSommets; j++) {
 				if (areteExistante(graphe,i,j) == 0) {
 					/* 
 					* ... on regarde si un plus court chemin existe 
-					* pour le sommet d'arrivée
+					* pour le sommet d'arrivÃ©e
 					*/
 					if (taches[j-1]->dateTot < taches[i-1]->dateTot + poidsArete(graphe,i,j)) {
 						taches[j-1]->dateTot = taches[i-1]->dateTot + poidsArete(graphe,i,j);
@@ -214,24 +215,24 @@ static void calculDatesAuPlusTot(TypGraphePERT *graphePERT, int sommetDepart) {
 	/*
 	* Fonction : calculDatesAuPlusTard
 	*
-	* Paramètres : TypGraphePERT *graphePERT, pointeur sur un graphe PERT
+	* ParamÃ¨tres : TypGraphePERT *graphePERT, pointeur sur un graphe PERT
 	*
 	* Description : Calcule les dates au plus tard d'un graphe de toutes les
-	*				tâches du graphe PERT passé en paramètre.
-	*				Cette fonction suppose avoir déjà calculé les dates au plus
-	*				tôt de toutes les tâches du graphe.
+	*				tÃ¢ches du graphe PERT passÃ© en paramÃ¨tre.
+	*				Cette fonction suppose avoir dÃ©jÃ  calculÃ© les dates au plus
+	*				tÃ´t de toutes les tÃ¢ches du graphe.
 	*/
 static void calculDatesAuPlusTard(TypGraphePERT *graphePERT) {
-	TypGraphePERT *graphePERTInverse;  /* Le graphe PERT inversé */
-	TypTache      **taches;            /* Les tâches associées au graphe PERT */
-	TypTache      **tachesInverse;     /* Les tâches associées au graphe inversé */
+	TypGraphePERT *graphePERTInverse;  /* Le graphe PERT inversÃ© */
+	TypTache      **taches;            /* Les tÃ¢ches associÃ©es au graphe PERT */
+	TypTache      **tachesInverse;     /* Les tÃ¢ches associÃ©es au graphe inversÃ© */
 	int           nbSommets;           /* Le nombre de sommets du graphe */
 	int           i;                   /* Permet le parcours des sommets */
 	
 	taches = graphePERT->taches;
 	nbSommets = graphePERT->graphe->nbrMaxSommets;
 	
-	/* Création du graphe inversé */
+	/* CrÃ©ation du graphe inversÃ© */
 	tachesInverse = malloc((nbSommets-2) * sizeof(TypTache));
 	for (i = 0; i <= nbSommets - 3; i++) {
 		tachesInverse[i] = creerTache(taches[i]->nom,taches[i]->intitule,taches[i]->duree,taches[i]->dependances);
@@ -239,13 +240,13 @@ static void calculDatesAuPlusTard(TypGraphePERT *graphePERT) {
 	graphePERTInverse = creerGraphePERT(tachesInverse,nbSommets-2);
 	graphePERTInverse->graphe = grapheInverse(graphePERTInverse->graphe);
 	
-	/* On calcule les dates au plus tôt du graphe inversé */
+	/* On calcule les dates au plus tÃ´t du graphe inversÃ© */
 	calculDatesAuPlusTot(graphePERTInverse,nbSommets);
 	
 	/*
-	* Soit t"(i) la date au plus tard de la tâche i,
-	* t'(i) la date au plus tôt de la tâche i dans le graphe inversé,
-	* t(omega) la date de la dernière tâche du graphe.
+	* Soit t"(i) la date au plus tard de la tÃ¢che i,
+	* t'(i) la date au plus tÃ´t de la tÃ¢che i dans le graphe inversÃ©,
+	* t(omega) la date de la derniÃ¨re tÃ¢che du graphe.
 	*
 	* Alors : t"(i) = t(omega) - t'(i)
 	*/
@@ -261,12 +262,12 @@ static void calculDatesAuPlusTard(TypGraphePERT *graphePERT) {
 	/*
 	* Fonction : dureeTotale
 	*
-	* Paramètres : TypGraphePERT *graphePERT, pointeur sur un graphe PERT
+	* ParamÃ¨tres : TypGraphePERT *graphePERT, pointeur sur un graphe PERT
 	*
-	* Retour : int, la durée totale du chantier
+	* Retour : int, la durÃ©e totale du chantier
 	*
-	* Description : Renvoie la durée totale du chantier représenté par le
-	*				graphe PERT passé en paramètre
+	* Description : Renvoie la durÃ©e totale du chantier reprÃ©sentÃ© par le
+	*				graphe PERT passÃ© en paramÃ¨tre
 	*/
 int dureeTotale(TypGraphePERT *graphePERT) {
 	return graphePERT->taches[graphePERT->graphe->nbrMaxSommets - 1]->dateTot;
@@ -276,17 +277,17 @@ int dureeTotale(TypGraphePERT *graphePERT) {
 	/*
 	* Fonction : afficherDates
 	*
-	* Paramètres : TypGraphePERT *graphePERT, pointeur sur un graphe PERT
+	* ParamÃ¨tres : TypGraphePERT *graphePERT, pointeur sur un graphe PERT
 	*
-	* Description : Affiche les dates des tâches du graphe sous la forme suivante :
+	* Description : Affiche les dates des tÃ¢ches du graphe sous la forme suivante :
 	*
 	*				# nom, date au plus tot, date au plus tard
 	*				A, 2, 5
 	*				B, 7, 9
 	*/
 void afficherDates(TypGraphePERT *graphePERT) {
-	TypTache **taches; /* Les tâches associées au graphe PERT */
-	int      i;        /* Permet le parcours des tâches */
+	TypTache **taches; /* Les tÃ¢ches associÃ©es au graphe PERT */
+	int      i;        /* Permet le parcours des tÃ¢ches */
 	
 	taches = graphePERT->taches;
 	
@@ -299,21 +300,101 @@ void afficherDates(TypGraphePERT *graphePERT) {
 	}
 }
 
+	/*
+	* Fonction : afficherCheminCritique
+	*
+	* ParamÃ¨tres : TypGraphePERT *graphePERT, pointeur sur un graphe PERT
+	*
+	* Description : Affiche le Chemin critique sous la forme :
+	*
+	*				# nomTache1 -> nomTache2 -> .....
+	*				A -> B -> C -> D
+	*				
+	*/
 
-void afficherCheminCritique(TypGraphePERT* graphe){}
+void afficherCheminCritique(TypGraphePERT* graphePERT){
+	TypTache      **taches;            /* Les tÃ¢ches associÃ©es au graphe PERT */
+	int 		i;		/* variable de parcours des tÃ¢ches du graphe*/
+	int 		tmp1;		/* date au plus tÃ´t de la tÃ¢che courante */
+	int 		tmp2;		/* ate au plus tard de la tÃ¢che courante */
+    int         cmp;        /* Le nombre de sommets trouvÃ©s sur le chemin critique */
 
-TypGraphePERT* lireGraphePERT(FILE* file){}
+    cmp = 0;
+	taches = graphePERT->taches;
+	printf("Chemin critique correspondant au graphe en cours :\n");
+
+	for(i = 1; i <= graphePERT->graphe->nbrMaxSommets - 2; i++){
+
+		tmp1 = taches[i-1]->dateTot;
+		tmp2 = taches[i-1]->dateTard;
+
+        if(tmp1 == tmp2){
+            if (cmp != 0) {
+                printf(" --> ");
+            }
+            
+            printf("%c",taches[i-1]->nom);
+            cmp++;
+        }
+	}
+}
+
+
+	/*
+	* Fonction : lireGraphePERT
+	*
+	* ParamÃ¨tres : char nom
+	*
+	* Retour : 0
+	*
+	* Description : Affiche le fichier chantier.txt sous la forme:
+	*
+	*	# nom, â€™intituleâ€™, duree, dependances
+	*	#--------------------------------------
+ 	*	 A, â€™maconnerieâ€™, 7, -
+ 	*	 B, â€™charpenteâ€™, 4, A
+	* 	 C, â€™toitureâ€™, 2, -
+	*	 D, â€™sanitairesâ€™, 6, A, C
+	*	 E, â€™electriciteâ€™, 2, B
+	*	 F, â€™fenetreâ€™, 1, C, D, E
+	*	 G, â€™abordsâ€™, 1, D, E
+	*	 H, â€™plafondâ€™, 1, F
+	*	 I, â€™facadeâ€™, 4, D, E
+	*	 J, â€™peintureâ€™, 2, H
+	*	 K, â€™finitionsâ€™, 1, G, I, J
+	*/
+
+int lireGraphePERT(char nomFichier[80]){
+
+	char buffer, chemin[80] = "lecture/";
+	FILE *fichier = NULL;
+	strcat ( chemin, nomFichier );
+	fichier = fopen ( chemin, "r+" );/*Ouverture du fichier en mode lecture*/
+	if ( fichier == NULL ) {
+		return 0;
+	}
+
+	else{
+		while((buffer = fgetc(fichier)) != EOF){
+			printf("%c",buffer);
+		}
+	fclose(fichier);
+	return 0;
+
+	}
+
+}
 
 
 	/*
 	* Fonction : tacheEnSommet
 	*
-	* Paramètres : char nom, le nom d'une tâche
+	* ParamÃ¨tres : char nom, le nom d'une tÃ¢che
 	*
-	* Retour : int, le numéro du sommet correspondant
+	* Retour : int, le numÃ©ro du sommet correspondant
 	*
-	* Description : Renvoie le numéro du sommet correspondant au nom
-	*               de la tâche. On considère que :
+	* Description : Renvoie le numÃ©ro du sommet correspondant au nom
+	*               de la tÃ¢che. On considÃ¨re que :
 	*               'A' <-> 1
 	*               'B' <-> 2  etc...
 	*/
@@ -325,12 +406,12 @@ static int tacheEnSommet(char nom) {
 	/*
 	* Fonction : sommetEnTache
 	*
-	* Paramètres : int sommet, le numéro d'un sommet d'un graphe
+	* ParamÃ¨tres : int sommet, le numÃ©ro d'un sommet d'un graphe
 	*
-	* Retour : char, le nom de la tâche correspondante
+	* Retour : char, le nom de la tÃ¢che correspondante
 	*
-	* Description : Renvoie le nom de la tâche correspondant au numéro
-	*               du sommet. On considère que :
+	* Description : Renvoie le nom de la tÃ¢che correspondant au numÃ©ro
+	*               du sommet. On considÃ¨re que :
 	*               1 <-> 'A'
 	*               2 <-> 'B'  etc...
 	*/
